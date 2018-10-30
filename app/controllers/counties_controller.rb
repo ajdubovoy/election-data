7a60ba7867e7c77a622c372624f0e90047d07281
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-10-30 00:33:00 -0700

    fix bug for no search

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -1,9 +1,11 @@
 class CountiesController < ApplicationController
   def index
     if params['county']
-      @county = County.find(params['county']['fips'].to_i)
       @state = State.find(params['state_id'].to_i)
-      redirect_to state_county_path(@county, state_id: @state.fips)
+      redirect_to state_path(@state) if params['county']['fips'].empty?
+      @county = County.find(params['county']['fips'].to_i) unless params['county']['fips'].empty?
+      redirect_to state_county_path(@county, state_id: @state.fips) unless params['county']['fips'].empty?
+
     end
   end
 
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,7 @@
 class StatesController < ApplicationController
   def index
     @states = State.all
-    if params["state"]
+    if params["state"].present? && params["state"]["fips"].present?
       @state = State.find(params['state']['fips'].to_i)
       redirect_to state_path(@state)
     end
