65c568bebbf6e8a8208527451b1367ac5c65cfa1
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-10-31 14:34:38 -0700

    implement authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  before_action :authenticate_everything, :if => lambda { !%w{ development test production loadtest }.include?(Rails.env) }
+
+  def authenticate_everything
+    authenticate_or_request_with_http_basic("TrustTheVote") do |user, password|
+      pass = Settings.admin_password
+      pass.present? && user == 'trustthevote' && password == 'mid18term'
+    end
+  end
 end
