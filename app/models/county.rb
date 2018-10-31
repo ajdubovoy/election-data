192ca016b4cfdfb9639f747d433bbc462255c3ec
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-10-31 15:56:07 -0700

    implement audit status

--- app/models/county.rb
+++ app/models/county.rb
@@ -19,21 +19,4 @@ class County < ApplicationRecord
     else 'No data provided'
     end
   end
-
-  def audit_status_pretty
-    case self.audit_status
-    when 'NotRequiredAsPaper' then 'No Required Audits / Machine-Based Audits'
-    when 'Paperless' then 'Paperless Voting'
-    when 'RequiredPaper' then 'Required Manual Audit of Paper Ballots'
-    else 'No data provided'
-    end
-  end
-
-  def audit_status_color
-    case self.audit_status
-    when 'NotRequiredAsPaper' then 'amb'
-    when 'Paperless' then 'bad'
-    when 'RequiredPaper' then 'good'
-    end
-  end
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,20 @@
 class State < ApplicationRecord
   has_many :counties
+
+  def audit_status_pretty
+    case self.audit_status
+    when 'NotRequiredAsPaper' then 'No Required Audits / Machine-Based Audits'
+    when 'Paperless' then 'Paperless Voting'
+    when 'RequiredPaper' then 'Required Manual Audit of Paper Ballots'
+    else 'No data provided'
+    end
+  end
+
+  def audit_status_color
+    case self.audit_status
+    when 'NotRequiredAsPaper' then 'amb'
+    when 'Paperless' then 'bad'
+    when 'RequiredPaper' then 'good'
+    end
+  end
 end
