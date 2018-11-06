cd97068782daef0250358dd34ca4513422bed901
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-11-06 17:50:51 +0900

    fix poll book migration

--- db/migrate/20181106085034_add_poll_book_to_states.rb
+++ db/migrate/20181106085034_add_poll_book_to_states.rb
@@ -1,4 +1,5 @@
 class AddPollBookToStates < ActiveRecord::Migration[5.2]
   def change
+    add_column :states, :poll_book, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_084541) do
+ActiveRecord::Schema.define(version: 2018_11_06_085034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_084541) do
     t.string "ease"
     t.string "early_voting"
     t.string "voter_id"
+    t.string "poll_book"
   end
 
 end
